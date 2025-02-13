6628e1b5a3a1e858b63970e00f94fbdd330f4d52
Yamil <issayamilrt@gmail.com>
2025-02-13 09:43:29 +0100

    use typescript

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -1,26 +1,40 @@
-const { ModuleFederationPlugin } = require("webpack").container;
-const path = require("path");
+const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main.jsx",
-  mode: "development",
+  entry: './src/main.tsx',
+  mode: 'development',
   devServer: {
     port: 3000,
-    historyApiFallback: true,
   },
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: 'auto',
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        },
+      },
+    ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "shell",
+      name: 'shell',
       remotes: {
-        headerMfe: "headerMfe@http://localhost:3001/remoteEntry.js",
+        headerMfe: 'headerMfe@http://localhost:3001/remoteEntry.js',
       },
-      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
     }),
   ],
 };
